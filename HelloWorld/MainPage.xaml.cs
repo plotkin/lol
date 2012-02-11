5c2f38f89e38700701c3173b9ec868ba57ada9c5
plotkin <dmitr.gridnev@gmail.com>
2012-02-11 15:05:49 +0400

    lol

---
+++ HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+
+namespace HelloWorld
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Конструктор
+        public MainPage()
+        {
+            InitializeComponent();
+            name.Text = Orientation.ToString();
+        }
+
+
+
+        private void ContentPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            name.Text = string.Format("Windows Phone 7 - its really comfortable platform\n", name.Width);
+        }
+
+        private void PhoneApplicationPage_OrientationChanged(object sender, OrientationChangedEventArgs e)
+        {
+            name.Text = e.Orientation.ToString();
+            base.OnOrientationChanged(e);
+        }
+    }
+}
